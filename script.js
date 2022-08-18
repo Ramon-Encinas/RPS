54dc37691452de4f7d6990b07cc16965f59c53f8
Ramon <ramon.enra@gmail.com>
2022-08-18 15:30:23 -0700

    Game now ends when someone reaches 5 points, Added UI that works with buttons, Game can no longer end in draws,

--- script.js
+++ script.js
@@ -5,19 +5,45 @@ function computerPlay() {
     const randomChoice = Math.floor(Math.random()*myArray.length);
     const computerChoice = myArray[randomChoice];
     return computerChoice;
-}
+}    
+let pointArray = [0, 0, 0];
 
-let pointArray = [0, 0];
+let dude = '';
+
+const btn1 = document.querySelector('.choice1');
+btn1.addEventListener('click', () => {
+    playerChoice = "ROCK";
+    playRound();
+    game();
+    let output = document.getElementById("score");
+    output.innerHTML = pointArray;} )
+    
+const btn2 = document.querySelector('.choice2');
+btn2.addEventListener('click', () => {
+    playerChoice = "PAPER";
+    playRound()
+    game();
+    let output = document.getElementById("score");
+    output.innerHTML = pointArray;} )
+
+const btn3 = document.querySelector('.choice3');
+btn3.addEventListener('click', () => {
+    playerChoice = "SCISSORS";
+    playRound();
+    game();
+     let output = document.getElementById("score");
+    output.innerHTML = pointArray;} )
 
 
-function playRound(playerSelection, computerSelection) {
-    console.log(playerSelection = prompt("Choose your move: ").toUpperCase());
+function playRound(playerSelection) {
+    playerSelection = playerChoice;
+    console.log(playerSelection); 
     console.log(computerSelection = computerPlay().toUpperCase());
-
 
 
     if (playerSelection == computerSelection) {
         console.log("It is a draw!");
+        pointArray[2] += 1;
 
     }   else if (playerSelection == "ROCK" && computerSelection == "PAPER") {
         console.log("Computer wins!, Paper beats Rock!");
@@ -45,29 +71,19 @@ function playRound(playerSelection, computerSelection) {
 
     }   else { 
         console.log("Error, player choice is not valid: Please type in 'rock', 'paper' or 'scissors'");
-    }
+    }    
 
-    console.log(pointArray)
-}
+    console.log(pointArray);
+
+}    
 
 function game() {
 
-    for (let i = 0; i < 5; i++) {
-        playRound()
+    if (pointArray[0] > pointArray[1] && pointArray[0] == 5) {
+            alert("Player has won!");
+    }   else if (pointArray[0] < pointArray[1] && pointArray[1] == 5) {
+            alert("Computer has won!");
     }
-
-    if (pointArray[0] > pointArray[1]) {
-        alert("Player has won!")
-    }   else if (pointArray[0] < pointArray[1]) {
-        alert("Computer has won!")
-    }   else {
-        alert("No one won!")
-    }
-
+    
 }
-
-console.log(game())
-
-
-
-
+ 
